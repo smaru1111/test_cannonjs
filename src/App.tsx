e4ba9982f77c578ef00de9d268723135961d1d2c
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-09-16 11:59:22 +0000

    chain削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+/** @jsxImportSource @emotion/react */
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Text1 from './pages/Test1';
+import Chain from './pages/Chain';
 import { css } from '@emotion/react';
+import Trigger from './pages/Trigger';
+import Fog from './pages/Fog';
 const style = css`
   position: absolute;
   top: 0;
@@ -11,19 +15,30 @@ const style = css`
   padding: 10px;
   border-radius: 10px;
 `;
+const wrapper = css`
+  position: relative;
+`;
 
 
 const App = () => {
   return (
     <>
-      <div css={style}>
-        <Link to="/">Home</Link>
-        <Link to="/test1">Test1</Link>
+      <div css={wrapper}>
+        <div css={style}>
+          <Link to="/">Home</Link>
+          <Link to="/test1">Test1</Link>
+          <Link to="/chain">Chain</Link>
+          <Link to="/trigger">Trigger</Link>
+          <Link to="/fog">Fog</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test1" element={<Text1 />} />
+          <Route path="/chain" element={<Chain />} />
+          <Route path="/trigger" element={<Trigger/>} /> 
+          <Route path="/fog" element={<Fog />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test1" element={<Text1 />} />
-      </Routes>
     </>
   );
 }
